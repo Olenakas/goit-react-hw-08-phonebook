42911e6b6a74d5599a5e69f282fb9dc7a528e4f7
Olena Kasianenko <kasianenko.elena.a@gmail.com>
2023-09-10 07:49:46 +0200

    commit

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -59,7 +59,7 @@ const RegisterForm = () => {
               type="text"
               value={name}
               onChange={handleChange}
-              pattern="^([A-Za-z-']{1,50})|([А-Яа-я-']{1,50})$"
+              pattern="^([A-Za-z'-]{1,50})|([А-Яа-я'-]{1,50})$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
               required
               sx={{ mb: 2 }}
@@ -73,7 +73,7 @@ const RegisterForm = () => {
               type="email"
               value={email}
               onChange={handleChange}
-              pattern="^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$"
+              pattern="^[\w\.-]+@([\w-]+\.)+[\w-]{2,4}$"
               title="Email may contain only letters, apostrophe, dash and spaces."
               required
               sx={{ mb: 2 }}
